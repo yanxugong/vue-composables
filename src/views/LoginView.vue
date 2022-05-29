d1a0f4071d6ca6c4b8e1c49426123ce077f52ac1
yanxu gong <745710524@qq.com>
2022-05-29 16:35:15 +0800

    feat: Vue3 Composition API: useUser 实现登录功能封装、逻辑抽离和数据共享

---
+++ src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ElMessage } from "element-plus"
+import { computed, ref } from "vue"
+import { useUser } from "@/composables/useUser"
+
+const { loginModel, user, login, loggedIn, logout } = useUser()
+</script>
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <p v-if="loggedIn">
+      Welcome {{ user?.username }},
+      <a href="#" @click.prevent="logout">Logout</a>
+    </p>
+    <el-form v-if="!loggedIn" ref="form" :model="loginModel" label-width="80px">
+      <el-form-item label="username">
+        <el-input
+          v-model="loginModel.username"
+          placeholder="username"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="password">
+        <el-input
+          v-model="loginModel.password"
+          type="password"
+          placeholder="password"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">Login</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .login {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
